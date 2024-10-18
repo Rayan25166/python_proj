afa398ef003b6bd4e3cf6dd4226bead55af0e861
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-18 15:27:22 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the First Number: "))
+b=int(input("Enter the second number:"))
+print("sum: ",a+b,"Division: ",a/b)
+var1=(a>b)
+print("Is a greater than b?:",var1)
+var2=(a==b)
+print("Are a and b equal?:",var2)
+print("Logical And:",(a>b)and(b<a))
+print("Logical OR:",(a==b)or(a<b))
+
