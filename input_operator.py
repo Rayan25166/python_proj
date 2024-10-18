3f21923a90529798217f5328513926baba83182e
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-18 14:32:55 +0530

    Add files via upload

---
+++ input_operator.py
@@ -0,0 +1,20 @@
+'''
+Author:Rayan J
+ Date:18-10-2024
+ Description:Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+
+Enter your name: John
+
+Enter your age: 25
+
+Hello, John! You are 25 years old.
+'''
+
+
+name=input("Enter Your Name :")
+age=input("Enter Your Age :")
+
+
+print("Hello,",name,"! you are",age,"years old")
+
