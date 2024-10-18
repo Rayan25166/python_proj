81fe96464f9684ba6c8a3542d06e23d77b09a702
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-18 15:50:42 +0530

    Add files via upload

---
+++ basic1.py
@@ -0,0 +1,5 @@
+import math
+a=int(input("Enter the number:"))
+adf=print("square root of",a,":",math.sqrt(a))
+fgh=print("factorial of",a,":",math.factorial(a))
+jkj=print(a,"raised to the power 2:",a**2)
