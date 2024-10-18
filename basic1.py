4b3a5834efaeb1e0e028d3e453ad649c5b5682ca
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-18 16:18:47 +0530

    Update basic1.py

--- basic1.py
+++ basic1.py
@@ -2,4 +2,4 @@ import math
 a=int(input("Enter the number:"))
 adf=print("square root of",a,":",math.sqrt(a))
 fgh=print("factorial of",a,":",math.factorial(a))
-jkj=print(a,"raised to the power 2:",a**2)
+jkj=print(a,"raised to the power 2:",math.pow(a ,2))
