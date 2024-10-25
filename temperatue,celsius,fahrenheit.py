dd5855ac55906249d7427edd67f391577dd09be9
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-25 15:49:37 +0530

    Update temperatue,celsius,fahrenheit.py

--- temperatue,celsius,fahrenheit.py
+++ temperatue,celsius,fahrenheit.py
@@ -1,4 +1,10 @@
-
+'''
+Name:Rayan Joomy
+Date:25-10-2024
+Description:Write a Python program to convert temperature values back and forth between Celsius and Fahrenheit. 
+The user should be able to input a temperature in Celsius or Fahrenheit, 
+and the program should convert it to the other unit using the formula:
+'''
 temp1=int(input("Enter the temperature"))
 scale= input(" is this in (c)elcius or (f)ahrenheit?")
 
