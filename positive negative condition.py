1c8cea8abedfd79eca003c17ae97c87512e5f3cb
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-25 14:33:39 +0530

    Add files via upload

---
+++ positive negative condition.py
@@ -0,0 +1,15 @@
+'''
+Name:Rayan Joomy
+Date: 25-10-2024
+Description: Check whether a number is positive or not
+'''
+
+
+
+num1=int(input("Enter the first number"))
+
+if num1>0:
+    print("num1 is positive")
+else:
+    print("num1 is negative")
+
