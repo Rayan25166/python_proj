6eaa395d249e9c71c3b3bc94bf76d41c8dce8b47
Rayan25166 <rayanjoomy772@gmail.com>
2024-10-25 14:24:28 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,23 @@
+'''
+Name: Rayan Joomy
+date:25-10-2024
+Description:Write a Python program to find the largest of three numbers.
+The program should take three numbers as input from the user and determine which one is the largest.
+Use conditional statements to compare the numbers.
+'''
+
+
+
+
+number1=int(input("Enter the first number"))
+number2=int(input("Enter the second number"))
+number3=int(input("Enter the third number"))
+
+if number3 > number2:
+    print(number3, " is greater than " , number2)
+else:
+    print(number2, " is greater than " , number3)
+
+
+
+
