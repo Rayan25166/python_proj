680b93a1cb6c41c04906d44f452d91b97a1ecae9
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-29 14:25:42 +0530

    Add files via upload

---
+++ valid mobile number.py
@@ -0,0 +1,13 @@
+
+
+
+def is_valid_mobile_number(number):
+    if len(number)==10 and number[0] in["9","8","7"]:
+        return True
+    return False
+mobile_number=input("enter the mobile number")
+if is_valid_mobile_number(mobile_number):
+    print("the mobile number{mobile_number} is valid")
+else:
+    print("the mobile number{mobile_number} is invalid")
+
