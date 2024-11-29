a677a0a855e0dea96d92b2189f9b3841f57d6ac9
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-29 16:21:12 +0530

    Add files via upload

--- recursive function to add two numbers.py
+++ recursive function to add two numbers.py
@@ -1,6 +1,13 @@
-def recursive_add(num1,num2):
-    if num2==0:
-        return num1
+def addition(num_1,num_2):
+    if num_1==0 :
+        return num_2
+    elif num_2 ==0:
+        return num_1
     else:
-        return recursive_add(num1+1,num2-1)
-print(recursive_add(num1=2, num2=3))
+        return addition(num_1+1,num_2-1)
+
+numb_1 = int(input("Enter a number to find its addition: "))
+numb_2 = int(input("Enter second number to find its addition: "))
+
+result = addition(numb_1,numb_2)
+print(f"The addition of {numb_1} and {numb_2} is {result}")
