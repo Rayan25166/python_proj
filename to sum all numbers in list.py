91e0f57a2c2ae16c50a2954d2ac9b0c1bbd93382
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-30 15:55:06 +0530

    Add files via upload

---
+++ to sum all numbers in list.py
@@ -0,0 +1,14 @@
+'''
+Description:Write a Python function to sum all the numbers in a list.
+'''
+
+
+def sum_of_all_the_numbers(a,b,c,d,e):
+    total=0
+a=int(input("Enter the number"))
+b=int(input("Enter the number"))
+c=int(input("Enter the number"))
+d=int(input("Enter the number"))
+e=int(input("Enter the number"))
+print((sum((a,b,c,d,e))))
+sum_of_all_the_numbers(a,b,c,d,e)
