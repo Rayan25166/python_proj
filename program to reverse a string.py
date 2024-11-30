227bb44efe388352bae06551a5d77ef25f85f53e
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-30 15:52:23 +0530

    Add files via upload

---
+++ program to reverse a string.py
@@ -0,0 +1,8 @@
+''''
+Description:Write a Python program to reverse a string.
+'''
+def reverse_a_string(str):
+    str=[1,2,3,4,'a','b','c','d']
+    str.reverse()
+    print(str)
+reverse_a_string(str)
